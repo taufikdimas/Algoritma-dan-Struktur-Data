3364d05ae87c0750bb8fe45b199f373bae6f5a02
taufikdimas <taufikdimasedystara@gmail.com>
2024-02-28 15:27:25 +0700

    modify : latihan pratikum no 3

--- Pertemuan 3/mahasiswa_26.java
+++ Pertemuan 3/mahasiswa_26.java
@@ -34,5 +34,68 @@ public class mahasiswa_26 {
             System.out.println("Jenis Kelamin: " + jenisKelamin[i]);
             System.out.println("IPK: " + ipk[i]);
         }
+
+        // Hitung rata-rata
+        double rataRataIPK = hitungRataRataIPK(ipk);
+
+        // mahasiswa dengan IPK terbesar
+        Mahasiswa mhsIPKTertinggi = MahasiswaIPKTertinggi(nama, nim, jenisKelamin, ipk);
+
+        // Tampilkan data mahasiswa
+        System.out.println("\n\nData Mahasiswa");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("---------------------");
+            System.out.println("Nama: " + nama[i]);
+            System.out.println("NIM: " + nim[i]);
+            System.out.println("Jenis Kelamin: " + jenisKelamin[i]);
+            System.out.println("IPK: " + ipk[i]);
+        }
+
+        // Tampilkan rata-rata IPK
+        System.out.println("\nRata-rata IPK: " + rataRataIPK);
+
+        // Tampilkan data mahasiswa dengan IPK tertinggi
+        System.out.println("\nMahasiswa dengan IPK Tertinggi:");
+        System.out.println("Nama: " + mhsIPKTertinggi.nama);
+        System.out.println("NIM: " + mhsIPKTertinggi.nim);
+        System.out.println("Jenis Kelamin: " + mhsIPKTertinggi.jenisKelamin);
+        System.out.println("IPK: " + mhsIPKTertinggi.ipk);
+    }
+
+    // Method untuk menghitung rata-rata IPK
+    private static double hitungRataRataIPK(double[] ipk) {
+        double totalIPK = 0;
+        for (double d : ipk) {
+            totalIPK += d;
+        }
+        return totalIPK / ipk.length;
+    }
+
+    // Method mencari mahasiswa dengan IPK terbesar
+    private static Mahasiswa MahasiswaIPKTertinggi(String[] nama, String[] nim, String[] jenisKelamin,
+            double[] ipk) {
+        Mahasiswa mhsIPKTertinggi = new Mahasiswa(nama[0], nim[0], jenisKelamin[0], ipk[0]);
+        for (int i = 1; i < ipk.length; i++) {
+            if (ipk[i] > mhsIPKTertinggi.ipk) {
+                mhsIPKTertinggi = new Mahasiswa(nama[i], nim[i], jenisKelamin[i], ipk[i]);
+            }
+        }
+        return mhsIPKTertinggi;
+    }
+
+    // Class data mahasiswa
+    private static class Mahasiswa {
+        String nama;
+        String nim;
+        String jenisKelamin;
+        double ipk;
+
+        public Mahasiswa(String nama, String nim, String jenisKelamin, double ipk) {
+            this.nama = nama;
+            this.nim = nim;
+            this.jenisKelamin = jenisKelamin;
+            this.ipk = ipk;
+        }
+
     }
 }
