7fb038b9fe2a662b527ad213b27838c554a19e4d
taufikdimas <taufikdimasedystara@gmail.com>
2024-02-28 21:07:38 +0700

    modify : Percobaan 2 pertanyaan no 4

--- Pertemuan 3/ArrayObjects_26.java
+++ Pertemuan 3/ArrayObjects_26.java
@@ -18,6 +18,7 @@
 //             System.out.println("Persegi Panjang ke-" + i);
 //             System.out.println("Panjang: " + ppArray[i].panjang + ", Lebar: " + ppArray[i].lebar);
 //         }
+//Jobhseet Percobaan 1
 //         // ppArray[0] = new PersegiPanjang_26();
 //         // ppArray[0].panjang = 110;
 //         // ppArray[0].lebar = 30;
@@ -38,28 +39,27 @@
 //         // ", lebar: " + ppArray[2].lebar);
 //     }
 // }
+
 import java.util.Scanner;
 
 public class ArrayObjects_26 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        PersegiPanjang_26[][] ppArray = new PersegiPanjang_26[2][2];
-        for (int i = 0; i < ppArray.length; i++) {
-            for (int j = 0; j < ppArray[i].length; j++) {
-                ppArray[i][j] = new PersegiPanjang_26();
-                System.out.println("Persegi panjang baris ke-" + i + ", kolom ke-" + j);
-                System.out.print("Masukkan panjang: ");
-                ppArray[i][j].panjang = sc.nextInt();
-                System.out.print("Masukkan lebar: ");
-                ppArray[i][j].lebar = sc.nextInt();
-            }
-        }
+        // input panjang array
+        System.out.print("Masukkan jumlah Persegi Panjang : ");
+        PersegiPanjang_26[] ppArray = new PersegiPanjang_26[sc.nextInt()];
 
-        for (int i = 0; i < ppArray.length; i++) {
-            for (int j = 0; j < ppArray[i].length; j++) {
-                System.out.println("Persegi panjang baris ke-" + i + ", kolom ke-" + j);
-                System.out.println("Panjang: " + ppArray[i][j].panjang + ", Lebar: " + ppArray[i][j].lebar);
-            }
+        for (int i = 0; i < 3; i++) {
+            ppArray[i] = new PersegiPanjang_26();
+            System.out.println("Persegi Panjang ke-" + i);
+            System.out.print("Masukkan Panjang : ");
+            ppArray[i].panjang = sc.nextInt();
+            System.out.print("Masukkan Lebar : ");
+            ppArray[i].lebar = sc.nextInt();
+        }
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Persegi Panjang ke-" + i);
+            System.out.println("Panjang : " + ppArray[i].panjang + ", Lebar : " + ppArray[i].lebar);
         }
     }
 }
