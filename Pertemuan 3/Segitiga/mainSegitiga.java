55b8cc1d52d603633f4bbc20250ac1e64027af51
taufikdimas <taufikdimasedystara@gmail.com>
2024-02-29 10:46:27 +0700

    update : percobaan 3 pertanyaan no 4

---
+++ Pertemuan 3/Segitiga/mainSegitiga.java
@@ -0,0 +1,11 @@
+package Segitiga;
+
+public class mainSegitiga {
+    public static void main(String[] args) {
+        Segitiga[] sgArray = new Segitiga[4];
+        sgArray[0] = new Segitiga(10, 4);
+        sgArray[1] = new Segitiga(20, 10);
+        sgArray[2] = new Segitiga(15, 6);
+        sgArray[3] = new Segitiga(25, 10);
+    }
+}
