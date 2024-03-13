a2f27f26723b5e34afec9efbfe6502864259304c
taufikdimas <taufikdimasedystara@gmail.com>
2024-03-13 19:44:55 +0700

    update : percobaan 1 pertanyaan no 2

--- Pertemuan 5/minggu5/Faktorial_26.java
+++ Pertemuan 5/minggu5/Faktorial_26.java
@@ -4,10 +4,19 @@ public class Faktorial_26 {
 
     public int nilai;
 
-    int faktorialBF(int n) {
+    // int faktorialBF(int n) {
+    // int fakto = 1;
+    // for (int i = 1; i <= n; i++) {
+    // fakto = fakto * i;// i bukan 1
+    // }
+    // return fakto;
+    // }
+    int faktorialBF(int n) {// pertanyaan no 2 percobaan 1
         int fakto = 1;
-        for (int i = 1; i <= n; i++) {
-            fakto = fakto * i;// i bukan 1
+        int i = 1;
+        while (i <= n) {
+            fakto = fakto * i;
+            i++;
         }
         return fakto;
     }
