58022704889c2bd3529151d984ba45d16293bcc5
taufikdimas <taufikdimasedystara@gmail.com>
2024-04-03 12:21:06 +0700

    modify : pertanyaan percobaan 1

--- Pertemuan 8/Stack/Utama26.java
+++ Pertemuan 8/Stack/Utama26.java
@@ -5,14 +5,18 @@ import java.util.Scanner;
 public class Utama26 {
     public static void main(String[] args) {
         Scanner sc26 = new Scanner(System.in);
-        Gudang26 gudang = new Gudang26(7);
+        System.out.print("Masukkan kapasitas gudang: ");
+        int kapasitasGudang = sc26.nextInt();
+        sc26.nextLine();
+        Gudang26 gudang = new Gudang26(kapasitasGudang);
 
         while (true) {
             System.out.println("\nMenu :");
             System.out.println("1. Tambahkan Barang ke Gudang");
             System.out.println("2. Ambil Barang dari Gudang");
             System.out.println("3. Tampilkan Tumpukan Barang");
-            System.out.println("4. Keluar");
+            System.out.println("4. Lihat Barang Teratas");
+            System.out.println("5. Keluar");
             System.out.print("Masukkan Pilihan : ");
             int pilihan = sc26.nextInt();
             sc26.nextLine();
@@ -36,7 +40,14 @@ public class Utama26 {
                     gudang.tampilkanBarang();
                     break;
                 case 4:
+                    if (!gudang.cekKosong()) {
+                        System.out.println("Barang teratas: " + gudang.tumpukan[gudang.top].nama);
+                    } else {
+                        System.out.println("Tumpukan barang kosong");
+                    }
                     break;
+                case 5:
+                    System.exit(0);
                 default:
                     System.out.println("Pilihan tidak valid, Silahkan coba lagi");
             }
